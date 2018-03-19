4631a367e954e02ad0f519e9dd6ada62f3a69e29
Liam Prowse <itsliamprowse@gmail.com>
2018-03-19 20:07:35 +0000

    added the Catalog controllers and models

---
+++ Library/Models/Catalog/AssetIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using Library.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Catalog
+{
+    public class AssetIndexModel
+    {
+        public IEnumerable<AssetIndexListingModel> Assets { get; set; }
+    }
+}
